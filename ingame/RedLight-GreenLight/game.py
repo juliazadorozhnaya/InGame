df3e9e96132d850f11fd5a6e1e9a732c056ab00a
Yuliya <j.zadorozhnaya2707@gmail.com>
2022-06-12 18:02:30 +0300

    Game 3 is almost ready

---
+++ ingame/RedLight-GreenLight/game.py
@@ -0,0 +1,369 @@
+from Games.Mugunghwa import game_object
+from Games.game_settings import *
+
+import os
+
+AIM_LOCATION = "Mugunghwa/NPC/aim.png"
+BGM_LOCATION = "Mugunghwa/Sound/mugunghwa.mp3"
+BACKGROUND_LOCATION = "Mugunghwa/NPC/background.png"
+DOLL_BACK_LOCATION = "Mugunghwa/NPC/back.png"
+DOLL_FRONT_LOCATION = "Mugunghwa/NPC/front.png"
+SCREEN_STARTING_POINT = (0, 0)  # Upper left half of the results screen.
+STARTING_MESSAGE_Y_POS = (300, 400, 650)
+NPC_1_CODE = 1
+NPC_2_CODE = 2
+
+DEAD_MESSAGE = "dead"
+DOLL_MESSAGE = "DOLL"
+STARTING_LEVEL = 1
+KEY_INPUT = 768
+
+
+class Game:
+    """Creating a game window with all the characters."""
+    TICK_RATE = 90
+    TIMER_TIME = 5
+    NPC_CHANGE_DIRECTION_TIME = 1.9
+    AIM_CHANGE_DIRECTION_TIME = 1.5
+    GAME_OVER_TIMER = 30
+    NPC_1_SPEED = 4.1
+    NPC_2_SPEED = 3.7
+    AIM_SIZE = (500, 350)
+    AIM_SPEED = 2
+    TIMER_UNIT = 1000
+    LEVEL_UP_STEP = 0.3
+
+    def __init__(self, image_path, title, width, height, current_screen):
+
+        self.title = title
+        self.width = width
+        self.height = height
+        self.half_width = width / 2
+        self.one_third_screen = (width / 3, height / 3)
+        self.game_screen = pygame.display.set_mode(
+            (width, height), pygame.RESIZABLE
+        ) 
+
+        self.game_screen.fill(WHITE)
+        pygame.display.set_caption(title)  # Set the window title.
+        background_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(background_image, (width, height))
+
+        self.mugunghwa_timer = False  # Timer with tagger back.
+        self.game_over_timer = None
+        self.aim_image = pygame.image.load(get_abs_path(AIM_LOCATION))
+        self.ref_w, self.ref_h = self.game_screen.get_size()
+
+        self.npc_1_size = width / 10
+        self.npc_2_size = width / 8
+
+        self.doll = [
+            self.width * 0.456,
+            self.height / 80,
+            self.width / 8,
+            self.height * 0.1875,
+        ]
+
+        self.player_character_size = (width / 16, width / 11)
+        self.restart_message_y_pos = (180, 280)
+
+        self.volume_notice = True
+
+        try:
+            pygame.mixer.music.load(get_abs_path(BGM_LOCATION))
+        except Exception as e:
+            print(e)
+            print("Sound Load error")
+        pygame.display.set_mode(current_screen, pygame.RESIZABLE)
+
+    def create_npc(self, kind_of_npc, game_screen=None):
+
+        if game_screen is None:
+            game_screen = pygame.display.set_mode(
+                (SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE
+            )
+        if kind_of_npc == NPC_1_CODE:
+            size = self.npc_1_size
+        elif kind_of_npc == NPC_2_CODE:
+            size = self.npc_2_size
+
+        return game_object.NPC(size, size, kind_of_npc)
+
+    def start_game(self, level, score, select_mode):
+        score = self.run_game_loop(level, score, select_mode)
+        return score
+
+    def lose_game(self):
+        game_over_image = pygame.image.load(get_abs_path(GAME_OVER_LOCATION))
+        game_over_image = pygame.transform.scale(
+            game_over_image,
+            (
+                game_over_image.get_width()
+                * (self.game_screen.get_width() / SCREEN_WIDTH),
+                game_over_image.get_height()
+                * (self.game_screen.get_height() / SCREEN_HEIGHT),
+            ),
+        )
+        self.game_screen.blit(game_over_image, SCREEN_STARTING_POINT)
+        message_to_screen_center(
+            self.game_screen,
+            "You lose.",
+            RED,
+            large_font,
+            self.game_screen.get_width() / 4,
+            self.ref_w,
+            self.ref_h,
+        )
+        pygame.display.update()
+        clock.tick(0.5)
+
+    def run_game_loop(self, level, score, select_mode):
+        game_over = False
+        did_win = True
+
+        pygame.mixer.music.play(-1)
+        self.game_over_timer = GameOverTimer(self.GAME_OVER_TIMER)
+
+        player = game_object.PC(
+            self.half_width, self.height, *self.player_character_size
+        )
+
+        npc_1 = self.create_npc(NPC_1_CODE)
+        npc_2 = self.create_npc(NPC_2_CODE)
+        npc_1.BASE_SPEED = 1 + self.NPC_1_SPEED
+        npc_2.BASE_SPEED = 1 + self.NPC_2_SPEED
+        npcs = [npc_1, npc_2]
+
+        aim = game_object.Aim(*self.AIM_SIZE)
+        aim.BASE_SPEED *= self.AIM_SPEED
+
+        DOLL = game_object.GameObject(*self.doll)
+        DOLL.sprite_image(get_abs_path(DOLL_BACK_LOCATION))
+
+        start_ticks = pygame.time.get_ticks()
+        npc_ticks = pygame.time.get_ticks()
+        aim_ticks = pygame.time.get_ticks()
+
+        while not game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.mixer.music.stop()
+                    return
+
+            left_time = self.game_over_timer.time_checker()
+            if left_time < 0:
+                self.lose_game()
+                pygame.mixer.music.stop()
+                return
+
+            dir_x, dir_y = self.get_PC_dir()
+            self.game_screen.fill(WHITE)
+            self.image = pygame.transform.scale(
+                self.image,
+                (self.game_screen.get_width(), self.game_screen.get_height()),
+            )
+            self.game_screen.blit(self.image, SCREEN_STARTING_POINT)
+            DOLL.draw(self.game_screen)
+
+            for npc in npcs:
+                npc.move(self.width)
+                npc.draw(self.game_screen)
+            player.move(dir_x, dir_y, self.width, self.height)
+            player.draw(self.game_screen, dir_x, dir_y)
+
+            # The NPC turns the timer settings.
+            npc_elapsed_time = (pygame.time.get_ticks() - npc_ticks) / self.TIMER_UNIT
+            npc_timer = round(
+                float(self.NPC_CHANGE_DIRECTION_TIME - npc_elapsed_time), 1
+            )
+            if (
+                npc_timer <= 0
+            ):  # When npc_timer is 0, the direction of progress of all NPC changes.
+                for npc in npcs:
+                    npc.change_direction()
+                npc_ticks = pygame.time.get_ticks()
+                npc_elapsed_time = (
+                    pygame.time.get_ticks() - npc_ticks
+                ) / self.TIMER_UNIT
+
+            # Progress only when stop is false.
+            elapsed_time = (pygame.time.get_ticks() - start_ticks) / self.TIMER_UNIT
+            timer = round(float(self.TIMER_TIME - elapsed_time), 1)
+
+            # Current level, displaying the game end timer in the upper left corner of the screen.
+            message_to_screen_left(
+                self.game_screen,
+                "Level:" + str(level),
+                WHITE,
+                level_font,
+                self.game_screen.get_width() / 11,
+                self.game_screen.get_height() / 30,
+                self.ref_w,
+                self.ref_h,
+            )
+            message_to_screen_left(
+                self.game_screen,
+                "GAME OVER : " + str(left_time),
+                WHITE,
+                level_font,
+                self.game_screen.get_width() / 4.8,
+                self.game_screen.get_height() / 14,
+                self.ref_w,
+                self.ref_h,
+            )
+            message_to_screen_left(
+                self.game_screen,
+                "SCORE : " + str(round(score)),
+                BLACK,
+                level_font,
+                self.game_screen.get_width() / 1.2,
+                self.game_screen.get_height() / 23,
+                self.ref_w,
+                self.ref_h,
+            )
+
+            try:
+                collision = self.detect_all_collisions(
+                    player, npc_1, npc_2, DOLL, select_mode
+                )
+            except:
+                try:
+                    collision = self.detect_all_collisions(
+                        player, npc_1, 0, DOLL, select_mode
+                    )
+                except:
+                    collision = self.detect_all_collisions(
+                        player, 0, 0, DOLL, select_mode
+                    )
+
+            # Sound effects.
+            if pygame.mixer.music.get_busy() is False:
+                pygame.mixer.music.play(-1)
+
+            # The doll is preparing to turn around.
+            if timer <= 0:
+                self.volume_notice = False
+                # Resetting the timer.
+                DOLL.sprite_image(get_abs_path(DOLL_FRONT_LOCATION))
+                self.mugunghwa_timer = True
+                time = self.TIMER_TIME
+                time_checker = round(time - (timer) * (-1), 1)
+                if time_checker <= 0:
+                    DOLL.sprite_image(get_abs_path((DOLL_BACK_LOCATION)))
+                    self.mugunghwa_timer = False
+                    start_ticks = pygame.time.get_ticks()
+                    elapsed_time = (
+                        pygame.time.get_ticks() - start_ticks
+                    ) / self.TIMER_UNIT
+                    timer = round(float(self.TIMER_TIME - elapsed_time), 2)
+                else:
+
+                    aim.move(self.width)
+                    aim.draw(self.game_screen)
+                    aim_elapsed_time = (
+                        pygame.time.get_ticks() - aim_ticks
+                    ) / self.TIMER_UNIT
+                    aim_timer = round(
+                        float(self.AIM_CHANGE_DIRECTION_TIME - aim_elapsed_time), 1
+                    )
+                    if aim_timer <= 0:
+                        aim.change_direction()
+                        aim_ticks = pygame.time.get_ticks()
+                        aim_elapsed_time = (
+                            pygame.time.get_ticks() - aim_ticks
+                        ) / self.TIMER_UNIT
+
+                    if event.type == KEY_INPUT:
+                        did_win = False
+                        self.mugunghwa_timer = False  # The doll recovers to restart.
+                        DOLL.sprite_image(get_abs_path(DOLL_BACK_LOCATION))
+                        self.lose_game()
+                        break
+
+            if collision == DEAD_MESSAGE:
+                did_win = False
+                break
+            elif collision == DOLL_MESSAGE:
+                break
+
+
+            pygame.display.update()
+            clock.tick(self.TICK_RATE)
+
+        if did_win:
+            pygame.mixer.music.stop()
+            return left_time
+        else:
+            pygame.mixer.music.stop()
+            return
+
+    def get_PC_dir(self, dir_x=0, dir_y=0):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            dir_y = 1
+        if keys[pygame.K_DOWN]:
+            dir_y = -1
+        if keys[pygame.K_LEFT]:
+            dir_x = -1
+        if keys[pygame.K_RIGHT]:
+            dir_x = 1
+        return dir_x, dir_y
+
+    def detect_all_collisions(self, player, npc_1, npc_2, DOLL, select_mode):
+        dead = 0
+        dead += player.detect_collision(npc_2)
+        dead += player.detect_collision(npc_1)
+
+        if dead:
+            self.lose_game()
+            return DEAD_MESSAGE
+
+        # When colliding with the sight, we return DOLL_MESSAGE to transfer the winnings in the game to the parent function.
+        if player.detect_collision(DOLL):
+            message_to_screen_center(
+                self.game_screen,
+                "Pass!",
+                WHITE,
+                level_font,
+                self.game_screen.get_height() / 3,
+                self.ref_w,
+                self.ref_h,
+            )
+            if select_mode:
+                message_to_screen_center(
+                    self.game_screen,
+                    "Go to the next level.",
+                    WHITE,
+                    level_font,
+                    self.game_screen.get_height() / 2,
+                    self.ref_w,
+                    self.ref_h,
+                )
+                
+            else:
+                message_to_screen_center(
+                    self.game_screen,
+                    "Next level.",
+                    WHITE,
+                    level_font,
+                    self.game_screen.get_height() / 2,
+                    self.ref_w,
+                    self.ref_h,
+                )
+            pygame.display.update()
+            clock.tick(0.5)
+            return DOLL_MESSAGE
+
+
+# Start the game up
+def start_game(level, score, select_mode):
+    pygame.init()
+    current_screen = pygame.display.get_window_size()
+    new_game = Game(
+        get_abs_path(BACKGROUND_LOCATION),
+        SCREEN_TITLE,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        current_screen,
+    )
+    return new_game.start_game(level, score, select_mode)
---
+++ ingame/RedLight-GreenLight/game_object.py
@@ -0,0 +1,300 @@
+import os
+import random
+from Games.game_settings import *
+import pygame
+
+import os
+
+AIM_LOCATION = "NPC/aim.png"
+PC_FRONT_LOCATION = "PC/LinkFront.png"
+PC_BACK_LOCATION = "PC/LinkBack.png"
+PC_LEFT_LOCATION = "PC/LinkLeft.png"
+PC_RIGHT_LOCATION = "PC/LinkRight.png"
+DIRECTION_RANGE = (1, 5)  # Where can NPC players move to.
+
+
+def get_abs_path(path):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
+
+
+class GameObject:
+    def __init__(self, x, y, width, height, game_screen=None):
+
+        os.environ["SDL_VIDEODRIVER"] = "x11"
+        if not game_screen:
+            game_screen = pygame.display.set_mode(
+                (SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE
+            )
+
+        self.x_pos = x
+        self.y_pos = y
+        self.width = width
+        self.height = height
+        self.game_screen = game_screen
+
+    def sprite_image(self, image_path):
+        object_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(
+            object_image,
+            (
+                self.width * (self.game_screen.get_width() / SCREEN_WIDTH),
+                self.height * (self.game_screen.get_height() / SCREEN_HEIGHT),
+            ),
+        )
+
+    def draw(self, background):
+        self.image = pygame.transform.scale(
+            self.image,
+            (
+                self.width * (background.get_width() / SCREEN_WIDTH),
+                self.height * (background.get_height() / SCREEN_HEIGHT),
+            ),
+        )
+        background.blit(
+            self.image, (background.get_width() / 2.25, background.get_height() / 40)
+        )
+
+
+class NPC(GameObject):
+    BASE_SPEED = 1
+    
+    # Value for the Y coordinates needed to create the NPC.
+    NPC_1_Y_POS = 1 / 5
+    NPC_2_Y_POS = 3 / 7
+
+    def __init__(self, width, height, kind_of_object=1):
+        os.environ["SDL_VIDEODRIVER"] = "x11"
+        game_screen_size = pygame.display.get_window_size()
+        x_pos = game_screen_size[0] / 2  # Set NPC generation X coordinate to the center.
+        if kind_of_object == 1:
+            value = self.NPC_1_Y_POS
+        else: 
+            value = self.NPC_2_Y_POS
+
+        y_pos = game_screen_size[1] * value  # Set the Y coordinate of each NPC.
+
+        super().__init__(x_pos, y_pos, width, height)
+        self.kind_of_object = kind_of_object
+        object_image = pygame.image.load(get_abs_path(f"NPC/NPC{kind_of_object}.png"))
+        self.go_forward = False  
+        self.direction = 1  # (1 right, 2 left, 3 up, 4 down)
+        self.image = pygame.transform.scale(object_image, (width * (3 / 4), height))
+
+    def draw(self, background):
+        if self.go_forward:
+            self.image = pygame.transform.scale(
+                self.image,
+                (
+                    self.width * (background.get_width() / SCREEN_WIDTH) * (3 / 4),
+                    self.height * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            background.blit(
+                self.image,
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_width() / SCREEN_HEIGHT),
+                ),
+            )
+        else:
+            self.image = pygame.transform.scale(
+                self.image,
+                (
+                    self.width * (background.get_width() / SCREEN_WIDTH) * (3 / 4),
+                    self.height * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            background.blit(
+                pygame.transform.flip(self.image, 1, 0),
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_width() / SCREEN_HEIGHT),
+                ),
+            )
+
+    def move(self, max_width):
+        if self.x_pos <= 0:
+            self.direction = 1
+        elif self.x_pos >= max_width:
+            self.direction = 2
+        elif self.y_pos <= 0:
+            self.direction = 4
+        elif self.y_pos >= max_width:
+            self.direction = 3
+
+        if self.direction == 1:
+            self.x_pos += self.BASE_SPEED
+            self.go_forward = False
+        elif self.direction == 2:
+            self.x_pos -= self.BASE_SPEED
+            self.go_forward = True
+        elif self.direction == 3:
+            self.y_pos -= self.BASE_SPEED
+        else:
+            self.y_pos += self.BASE_SPEED
+
+    def change_direction(self):
+        self.direction = random.randrange(*DIRECTION_RANGE)
+
+
+class Aim(NPC):
+    """A class for creating a aim."""
+    BASE_SPEED = 3
+
+    def __init__(self, width, height, game_screen=None):
+        os.environ["SDL_VIDEODRIVER"] = "x11"
+        if game_screen is None:
+            game_screen = pygame.display.set_mode(
+                (SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE
+            )
+        super().__init__(width, height) 
+        object_image = pygame.image.load(get_abs_path(AIM_LOCATION))
+        self.image = pygame.transform.scale(
+            object_image,
+            (
+                self.width * (game_screen.get_width() / SCREEN_WIDTH),
+                self.height * (game_screen.get_height() / SCREEN_HEIGHT),
+            ),
+        )
+
+    def move(self, max_width):
+        super().move(max_width)
+        if self.x_pos <= 0:
+            self.direction = 1
+        elif self.x_pos >= max_width - self.width:
+            self.direction = 2
+        elif self.y_pos <= 0:
+            self.direction = 4
+        elif self.y_pos >= max_width - self.width:
+            self.direction = 3
+
+
+class PC(GameObject):  # Player character.
+    BASE_SPEED = 6
+    object_image = pygame.image.load(get_abs_path(PC_FRONT_LOCATION))
+    player_character = pygame.transform.scale(object_image, (40, 60))
+
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        # Download all skins for rotation.
+        object_image = pygame.image.load(get_abs_path(PC_BACK_LOCATION))
+        self.fr_image = pygame.transform.scale(object_image, (width, height))
+        object_image = pygame.image.load(get_abs_path(PC_FRONT_LOCATION))
+        self.ba_image = pygame.transform.scale(object_image, (width, height))
+        object_image = pygame.image.load(get_abs_path(PC_LEFT_LOCATION))
+        self.le_image = pygame.transform.scale(object_image, (width, height))
+        object_image = pygame.image.load(get_abs_path(PC_RIGHT_LOCATION))
+        self.ri_image = pygame.transform.scale(object_image, (width, height))
+
+#Draw all the skins by changing the direction of the player's movement using the move() function
+    def draw(self, background, dir_x, dir_y):
+        self.player_character = self.ba_image
+        self.ba_image = pygame.transform.scale(
+            self.ba_image,
+            (
+                self.width * (background.get_width() / SCREEN_WIDTH),
+                self.height * (background.get_height() / SCREEN_HEIGHT),
+            ),
+        )
+        if dir_y > 0:
+            self.fr_image = pygame.transform.scale(
+                self.fr_image,
+                (
+                    self.width * (background.get_width() / SCREEN_WIDTH),
+                    self.height * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            background.blit(
+                self.fr_image,
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            self.player_character = self.fr_image
+        elif dir_y < 0:
+            self.ba_image = pygame.transform.scale(
+                self.ba_image,
+                (
+                    self.width * (background.get_width() / SCREEN_WIDTH),
+                    self.height * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            background.blit(
+                self.ba_image,
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            self.player_character = self.ba_image
+        elif dir_x > 0:
+            self.ri_image = pygame.transform.scale(
+                self.ri_image,
+                (
+                    self.width * (background.get_width() / SCREEN_WIDTH),
+                    self.height * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            background.blit(
+                self.ri_image,
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            self.player_character = self.ri_image
+        elif dir_x < 0:
+            self.le_image = pygame.transform.scale(
+                self.le_image,
+                (
+                    self.width * (background.get_width() / SCREEN_WIDTH),
+                    self.height * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            background.blit(
+                self.le_image,
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+            self.player_character = self.le_image
+        else:
+            background.blit(
+                self.player_character,
+                (
+                    self.x_pos * (background.get_width() / SCREEN_WIDTH),
+                    self.y_pos * (background.get_height() / SCREEN_HEIGHT),
+                ),
+            )
+# Change direction according to keystrokes.
+    def move(self, dir_x, dir_y, max_width, max_height):
+        
+        MOVE_BY = self.BASE_SPEED
+    # Changing the direction diagonally.
+        if dir_x != 0 and dir_y != 0:
+            MOVE_BY *= 0.707
+        # Define X and Y  movement.
+        self.y_pos += MOVE_BY * -dir_y
+        self.x_pos += MOVE_BY * dir_x
+        # Boundary detection.
+        if self.y_pos > max_height - self.height:
+            self.y_pos = max_height - self.height
+        elif self.y_pos < 0:
+            self.y_pos = 0
+        if self.x_pos > max_width - self.width:
+            self.x_pos = max_width - self.width
+        elif self.x_pos < 0:
+            self.x_pos = 0
+
+    def detect_collision(self, other_body):
+        if self.y_pos > other_body.y_pos + other_body.height - self.height / 2:
+            return False
+        elif self.y_pos + self.height < other_body.y_pos:
+            return False
+        if self.x_pos > other_body.x_pos + other_body.width:
+            return False
+        elif self.x_pos + self.width < other_body.x_pos:
+            return False
+        return True
