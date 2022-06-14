02b05c2e74a84a60de4619ca69b7bea18d475c2d
Anna Chistyakova <ann24111@mail.ru>
2022-06-14 12:36:24 +0300

    Add init docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,19 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../ingame'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'InGame'
-copyright = '2022, Anna, Julia'
+copyright = '2022, Anna Chistyakova, Julia Zadorozhnaya'
 author = 'Anna Chistyakova, Julia Zadorozhnaya'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '2022'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +30,9 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
